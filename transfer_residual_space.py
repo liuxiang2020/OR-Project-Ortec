81286870732e1878438f609f5d6063a2bc20b165
ks817614 <kevin.schwarz@rwth-aachen.de>
2020-05-01 22:11:03 +0200

    Need to verify

---
+++ transfer_residual_space.py
@@ -0,0 +1,18 @@
+#spaceStack = [[[0,10,0],[10,15,30]], [[10,0,0], [50,25,30]]]
+
+def create_residual_space(spaceStack):
+    curSpace = spaceStack.pop()
+    nextSpace = spaceStack.pop()
+    interSec_x = nextSpace[0][0] + nextSpace[1][0]
+    interSec_y = nextSpace[0][1] + nextSpace[1][1]
+    interSec_z = nextSpace[0][2] + nextSpace[1][2]
+    if curSpace[0][0] == interSec_x:
+        nextSpace[1][0] += curSpace[1][0]
+    elif curSpace[0][1] == interSec_y:
+        nextSpace[1][1] += curSpace[1][1]
+    elif curSpace[0][2] == interSec_z:
+        nextSpace[1][2] += curSpace[1][2]
+    spaceStack.append(nextSpace)
+    return spaceStack
+
+#print(create_residual_space(spaceStack))
