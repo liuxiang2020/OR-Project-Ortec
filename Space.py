6c766c8de8e5abc47cd2a7ea5fc8ede3e29272d8
Daniel Bugdalle <daniel.bugdalle@rwth-aachen.de>
2020-05-03 13:16:33 +0200

    Added datatype 'Space' Changed create_residual_space.py accordingly

---
+++ Space.py
@@ -0,0 +1,17 @@
+class Space:
+    def __init__(self, corner, size, kind):
+        self.corner = corner
+        self.size = size
+        self.kind = kind
+    
+    def get_corner(self):
+        return self.corner
+    
+    def get_size(self):
+        return self.size
+    
+    def get_kind(self):
+        return self.kind
+    
+    def __repr__(self):
+        return "[%s,%s,"% (self.corner , self.size)+self.kind+"]" 
--- create_residual_space.py
+++ create_residual_space.py
@@ -1,27 +1,29 @@
-#space: at pos (0,1,0), size (55,34,34)
-#spaceStack[[0,1,0][55,34,34]]
+from Space import Space
 
 def create_residual_space(blockSize, containerSize, spaceStack):
     #space = spaceStack.top()
     space = spaceStack.pop()
-    resX =[[0,0,0],[0,0,0]]
-    resY =[[0,0,0],[0,0,0]]
-    resZ =[[0,0,0],[0,0,0]]
-    w=space[1][0]
-    l=space[1][1]
-    h=space[1][2]
+    cornerx=[0,0,0]
+    cornery=[0,0,0]
+    cornerz=[0,0,0]
+    sizex=[0,0,0]
+    sizey=[0,0,0]
+    sizez=[0,0,0]
+    w=space.get_size()[0]
+    l=space.get_size()[1]
+    h=space.get_size()[2]
     deltaw = w-blockSize[0]
     deltal = l-blockSize[1]
     deltah = h-blockSize[2]
-    resZ[1][0] = blockSize[0]
-    resZ[1][1] = blockSize[1]
-    resZ[1][2] = deltah
-    x = space[0][0]
-    y = space[0][1]
-    z = space[0][2]
-    resX[0][2]=z
-    resY[0][2]=z
-    resZ[0][2]=z+blockSize[2] 
+    sizez[0] = blockSize[0]
+    sizez[1] = blockSize[1]
+    sizez[2] = deltah
+    x = space.get_corner()[0]
+    y = space.get_corner()[1]
+    z = space.get_corner()[2]
+    cornerx[2]=z
+    cornery[2]=z
+    cornerz[2]=z+blockSize[2] 
     #Corner Distinction
     corner1=False
     corner2=False
@@ -30,74 +32,74 @@ def create_residual_space(blockSize, containerSize, spaceStack):
     if (x+y <= y+abs(x+w-containerSize[0]) and
         x+y <= x+abs(y+l-containerSize[1]) and
         x+y <= abs(x+w-containerSize[0])+abs(y+l-containerSize[1])):
-        resX[0][0] = x + blockSize[0]
-        resX[0][1] = y
-        resY[0][0] = x
-        resY[0][1] = y + blockSize[1]
-        resZ[0][0] = x
-        resZ[0][1] = y
+        cornerx[0] = x + blockSize[0]
+        cornerx[1] = y
+        cornery[0] = x
+        cornery[1] = y + blockSize[1]
+        cornerz[0] = x
+        cornerz[1] = y
     #placed at corner nearest to [X,0,0]
     elif (y+abs(x+w-containerSize[0]) <= x+abs(y+l-containerSize[1]) and
           y+abs(x+w-containerSize[0]) <= abs(x+w-containerSize[0])+abs(y+l-containerSize[1])):
-        resX[0][0] = x
-        resX[0][1] = y
+        cornerx[0] = x
+        cornerx[1] = y
         corner1=True
-        resZ[0][0] = x + deltaw
-        resZ[0][1] = y
+        cornerz[0] = x + deltaw
+        cornerz[1] = y
     #placed at corner nearest to [0,Y,0]
     elif x+abs(y+l-containerSize[1]) <= abs(x+w-containerSize[0])+abs(y+l-containerSize[1]):
-        resY[0][0] = x
-        resY[0][1] = y
+        cornery[0] = x
+        cornery[1] = y
         corner2=True
-        resZ[0][0] = x
-        resZ[0][1] = y + deltal
+        cornerz[0] = x
+        cornerz[1] = y + deltal
     #placed at corner nearest to [X,Y,0]
     else:
         corner3=True
-        resZ[0][0] = x + deltaw
-        resZ[0][1] = y + deltal 
+        cornerz[0] = x + deltaw
+        cornerz[1] = y + deltal 
     # resX > resY?   
     if deltaw * l >= w*deltal:
-        resX[1][0] = deltaw
-        resX[1][1] = l
-        resX[1][2] = h
-        resY[1][0] = w - deltaw
-        resY[1][1] = deltal
-        resY[1][2] = h
+        sizex[0] = deltaw
+        sizex[1] = l
+        sizex[2] = h
+        sizey[0] = w - deltaw
+        sizey[1] = deltal
+        sizey[2] = h
         if corner1:
-            resY[0][0] = x + deltaw
-            resY[0][1] = y + blockSize[1]
+            cornery[0] = x + deltaw
+            cornery[1] = y + blockSize[1]
         elif corner2:
-            resX[0][0] = x + blockSize[0]
-            resX[0][1] = y 
+            cornerx[0] = x + blockSize[0]
+            cornerx[1] = y 
         elif corner3:
-            resX[0][0] = x
-            resX[0][1] = y 
-            resY[0][0] = x + deltaw
-            resY[0][1] = y 
-        spaceStack.append(resZ)
-        spaceStack.append(resY)
-        spaceStack.append(resX)
+            cornerx[0] = x
+            cornerx[1] = y 
+            cornery[0] = x + deltaw
+            cornery[1] = y 
+        spaceStack.append(Space(cornerz,sizez,'z'))
+        spaceStack.append(Space(cornery,sizey,'y'))
+        spaceStack.append(Space(cornerx,sizex,'x'))
     else:
-        resX[1][0] = deltaw
-        resX[1][1] = l - deltal
-        resX[1][2] = h
-        resY[1][0] = w
-        resY[1][1] = deltal
-        resY[1][2] = h
+        sizex[0] = deltaw
+        sizex[1] = l - deltal
+        sizex[2] = h
+        sizey[0] = w
+        sizey[1] = deltal
+        sizey[2] = h
         if corner1:
-            resY[0][0] = x
-            resY[0][1] = y + blockSize[1]
+            cornery[0] = x
+            cornery[1] = y + blockSize[1]
         elif corner2:
-            resX[0][0] = x + blockSize[0]
-            resX[0][1] = y + deltal
+            cornerx[0] = x + blockSize[0]
+            cornerx[1] = y + deltal
         elif corner3:
-            resX[0][0] = x
-            resX[0][1] = y + deltal
-            resY[0][0] = x
-            resY[0][1] = y
-        spaceStack.append(resZ)
-        spaceStack.append(resX)
-        spaceStack.append(resY)
+            cornerx[0] = x
+            cornerx[1] = y + deltal
+            cornery[0] = x
+            cornery[1] = y
+        spaceStack.append(Space(cornerz,sizez,'z'))
+        spaceStack.append(Space(cornerx,sizex,'x'))
+        spaceStack.append(Space(cornery,sizey,'y'))
     return spaceStack
     
