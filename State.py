b3b85cbf1a46012fcfb69df37d14b02e691365d8
Yaxin <yaxin.t@gmail.com>
2020-05-04 00:59:56 +0200

    add drafts of state.py, search_block.py, need further checked

---
+++ State.py
@@ -0,0 +1,37 @@
+class State:
+    def __init__(self, residualSpaceList, occupiedSpaceList, filledBlocks, utilization):
+        self.residualSpaceList = residualSpaceList
+        self.occupiedSpaceList = occupiedSpaceList
+        self.filledBlocks = filledBlocks
+        # u = 0
+        # for i in range(len(occupiedSpaceList)):
+        #     u += self.occupiedSpaceList[i].get_utilization()
+        self.utilization = utilization
+       
+    
+    def get_residualSpaceList(self):
+        return self.residualSpaceList
+    
+    def get_occupiedSpaceList(self):
+        return self.occupiedSpaceList
+
+    def get_filledBlocks(self):
+        return self.filledBlocks
+
+    def get_utilization(self):
+        return self.utilization
+    
+     def set_residualSpaceList(self, residualSpaceList):
+        return self.residualSpaceList = residualSpaceList
+    
+    def set_occupiedSpaceList(self,occupiedSpaceList):
+        return self.occupiedSpaceList = occupiedSpaceList
+
+    def set_filledBlocks(self, filledBlocks):
+        return self.filledBlocks = filledBlocks
+
+    def set_utilization(self,utilization):
+        return self.utilization = utilization
+    
+    # def __repr__(self):
+    #     return "[%s,%s,"% (self.corner , self.size)+self.kind+"]" 
--- search_block.py
+++ search_block.py
@@ -1,2 +1,41 @@
 import numpy as np
-def search_block():
+import Block.py
+import Space.py
+import State.py
+#from OR-Project-ORTEC import *
+"""
+NOTE: Space.py add information of filled blocks and utilization rate
+"""
+"""
+packState: A partial packing plan and a dynamically
+changing list of (List of (type)Spaces) residual spaces. 
+- residual spaces list
+- plan: occupied spaces list and filled blockList) ??maybe space utilization(seperate or total)) 
+
+candidateBlockList: a (type)List wil candiate (type)Blocks
+"""
+MAX_SIZE = 10
+def search_block(packState, candidateBlockList):
+    size = len(candidateBlockList) -1
+    # if only wants to limited the length of list, just limit the loop times?
+    # if size > MAX_SIZE:
+    #     size = MAX_SIZE 
+    bestIndex = -1
+    bestUtilization = -1    
+    for i in range(min(size, MAX_SIZE)):   
+        # curent packState needed to be considered contains 
+        # (residual space) and (used space and corresponding filled block) 
+        # and (? List of blocks that still feasible)
+
+        currState = packState
+        #first block from candidateBlockList, which has the highest fitness score
+        currBlock = candidateBlockList[i]
+        #Sol is a packState, assume it has a total utilization
+        Sol = Progressively_Refined_Tree_Search(currBlock, currState);
+        if Sol.get_utilization() > bestUtilisation:
+            bestIndex = i
+            bestUtilization = Sol.get_utilization()
+    #return the suitable block for the residual space 
+    return candidateBlockList[bestIndex]
+
+    
