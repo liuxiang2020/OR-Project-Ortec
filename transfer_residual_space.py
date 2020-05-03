084d7b6a11f3fd0e8dc5f9f36c9b9cf1450cb737
Daniel Bugdalle <daniel.bugdalle@rwth-aachen.de>
2020-05-03 15:10:32 +0200

    Changed transfer_residual_space.py for new Space type Added handling of the space corners

--- transfer_residual_space.py
+++ transfer_residual_space.py
@@ -1,15 +1,60 @@
-#spaceStack = [[[0,10,0],[10,15,30]], [[10,0,0], [50,25,30]]]
-
-def create_residual_space(spaceStack):
+from Space import Space
+def transfer_residual_space(spaceStack):
+    kindcheck = False
+    adjacentx = False
+    adjacenty = False
     curSpace = spaceStack.pop()
+    #can't do space transfer for z spaces
+    if curSpace.get_kind() == 'z':
+        return spaceStack
     nextSpace = spaceStack.pop()
-    interSec_x = nextSpace[0][0] + nextSpace[1][0]
-    interSec_y = nextSpace[0][1] + nextSpace[1][1]
-    if curSpace[0][0] == interSec_x:
-        nextSpace[1][0] += curSpace[1][0]
-    elif curSpace[0][1] == interSec_y:
-        nextSpace[1][1] += curSpace[1][1]
-    spaceStack.append(nextSpace)
-    return spaceStack
+    #no transfer between two spaces of the same kind or with a z space
+    kindcheck = curSpace.get_kind() == nextSpace.get_kind() or nextSpace.get_kind() == 'z' 
+    #adjacency checks
+    adjacentx = nextSpace.get_corner()[0] + nextSpace.get_size()[0] == curSpace.get_corner()[0] or curSpace.get_corner()[0] + curSpace.get_size()[0] == nextSpace.get_corner()[0]
+    adjacenty = nextSpace.get_corner()[1] + nextSpace.get_size()[1] == curSpace.get_corner()[1] or curSpace.get_corner()[1] + curSpace.get_size()[1] == nextSpace.get_corner()[1]
+    if kindcheck or (not(adjacentx) and curSpace.get_kind() == 'x') or (not(adjacenty) and curSpace.get_kind() == 'y'):
+        spaceStack.append(nextSpace)
+        return spaceStack
+    newcorner = [0,0,0]
+    newsize = [0,0,0]
+    #check for corner configuration
+    if(curSpace.get_kind()=='x'):
+        newsize = nextSpace.get_size()
+        newsize[0] += curSpace.get_size()[0]
+        if curSpace.get_corner()[1] < nextSpace.get_corner()[1]:
+            if curSpace.get_corner()[0] < nextSpace.get_corner()[0]:
+                #case1: block was at corner [X,0,0]
+                newcorner = nextSpace.get_corner()
+                newcorner[1] = curSpace.get_corner()[1]
+            elif curSpace.get_corner()[0] > nextSpace.get_corner()[0]:
+                #case2: block was at corner [0,0,0]
+                newcorner = nextSpace.get_corner()
+        elif curSpace.get_corner()[1]==nextSpace.get_corner()[1]:
+            if curSpace.get_corner()[0] < nextSpace.get_corner()[0]:
+                #case3: block was at corner [X,Y,0]
+                newcorner = curSpace.get_corner()
+            elif curSpace.get_corner()[0] > nextSpace.get_corner()[0]:
+                #case4: block was at corner [0,Y,0]
+                newcorner = nextSpace.get_corner()
+    if(curSpace.get_kind()=='y'):
+        newsize = nextSpace.get_size()
+        newsize[1] += curSpace.get_size()[1]
+        if curSpace.get_corner()[0] < nextSpace.get_corner()[0]:
+            if curSpace.get_corner()[1] < nextSpace.get_corner()[1]:
+                #case1: block was at corner [0,Y,0]
+                newcorner = nextSpace.get_corner()
+                newcorner[0] = curSpace.get_corner()[0]
+            elif curSpace.get_corner()[1] > nextSpace.get_corner()[1]:
+                #case2: block was at corner [0,0,0]
+                newcorner = nextSpace.get_corner()
+        elif curSpace.get_corner()[0]==nextSpace.get_corner()[0]:
+            if curSpace.get_corner()[1] < nextSpace.get_corner()[1]:
+                #case3: block was at corner [X,Y,0]
+                newcorner = curSpace.get_corner()
+            elif curSpace.get_corner()[1] > nextSpace.get_corner()[1]:
+                #case4: block was at corner [X,0,0]
+                newcorner = nextSpace.get_corner()
 
-#print(create_residual_space(spaceStack))
+    spaceStack.append(Space(newcorner,newsize,nextSpace.get_kind()))
+    return spaceStack
