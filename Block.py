eee192d53eb76948582d66da59fb2b6008d1df65
Yaxin <yaxin.t@gmail.com>
2020-05-09 13:23:26 +0200

    modifed block.py set_volume function, please check for additional changes

--- Block.py
+++ Block.py
@@ -1,13 +1,17 @@
 class Block:
     def __init__(self, block_id):
         self.id = block_id
-    
+        self.quantity = 0
+        self.volume = 0
+        self.size = 0
+        self.orientation = 0
+
     def get_id(self):
         return self.id
 
     def get_item_quantity(self):
         return self.quantity
-    
+
     def get_size(self):
         return self.size
 
@@ -16,25 +20,25 @@ class Block:
 
     def get_volume(self):
         return self.volume
-    
+
     def get_volume_loss(self):
         return self.volume_loss
 
     def set_item_quantity(self, quantity):
         self.quantity = quantity
-    
+
     def set_size(self, size):
         self.size = size
 
     def set_orientation(self, orientation):
         self.orientation = orientation
 
-    def set_volume(self, length, width, height):
-        self.volume = length* width* height
+    def set_volume(self, volume):
+        self.volume = volume
 
     def set_volume_loss(self, volume_loss):
         self.volume_loss = volume_loss
 
     def __repr__(self):
-        return "{id: %d\n, quantity:%d\n, volume: %d\n, size: %s\n, orientation:%s" \
-               % (self.id , self.quantity, self.volume, self.size, self.orientation )+"}\n"
+        return "{id: %s\n, quantity:%s\n, volume: %s\n, size: %s\n, orientation:%s" \
+               % (self.id, self.quantity, self.volume, self.size, self.orientation) + "}\n"
