ea50db4fd461a8bcbd2276bc48823c1d01bdf994
Yaxin <yaxin.t@gmail.com>
2020-05-10 00:09:33 +0200

    can compile and return a result, keep working on create_residual_space

--- test_algo.py
+++ test_algo.py
@@ -1,13 +1,26 @@
-# import numpy as np
+import numpy as np
 import inspect
 import sys
 import yaml
 from Functions import *
-import Block, Space, State
+from Block import Block
+from Space import Space
+from State import State
 from create_blocks import *
 from inspect import currentframe, getframeinfo
+from venv import *
+from create_general_candidate_block import *
+from create_residual_space import *
 import traceback
 
+"""
+Global Variables
+"""
+# store residual space
+spaceList = []
+# a dictionary of available items and its quantity
+availableItems = {}
+stateList = []
 """
 Returns the current line number in our program.
 """
@@ -45,8 +58,10 @@ def parse_yaml(yamlfile: yaml) -> object:
         itemKinds = file['data']['itemkinds']
         print(lineno(), "\n", containerSize, itemKinds)
         for i in range(len(itemKinds)):
+            availableItems[itemKinds[i]['id']] = itemKinds[i]['quantity']
             print(itemKinds[i]['quantity'])
         return containerSize, itemKinds
+
 
 
 if __name__ == "__main__":
@@ -56,22 +71,24 @@ if __name__ == "__main__":
     # parse_instance files
     containerSize, itemKinds = parse_yaml(instance)
 
+    # Global variables
+    print(availableItems)
+
     # check simple blocks
     simple_lst = create_simple_blocks(itemKinds, containerSize)
 
     # check general blocks
     general_lst = create_general_blocks(itemKinds, containerSize)
-    print(general_lst)
+    #print(general_lst)
+
+    # Build new space, start with empty container
+    s = Space([0,0,0],containerSize,'x')
+    spaceList.append(s)
+    # check candidate block
+    candidate_block_lst = generate_candidate_block_list(spaceList[0].get_size(), general_lst, availableItems)
+    # create residual space
+    spaceList= create_residual_space(candidate_block_lst[0].get_size(), containerSize, spaceList)
+    print(spaceList)
 
 
-
-
-
-
-
-
-
-
-    args = sys.argv
     print("Everything passed")
-    # print(sys.version)
