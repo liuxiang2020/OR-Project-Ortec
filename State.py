4e5044ba42bcd192367d5e7a9306bdc1ac135c69
yaxtang <33006597+yaxtang@users.noreply.github.com>
2020-05-14 11:10:15 +0200

    add space_volume and block_volume

--- State.py
+++ State.py
@@ -6,6 +6,7 @@ planListSpace: a list of filled Blocks eg [Block A, Block B]
 utilization: record current utilization rate
 available: record currently available items, it is a dictionary of id: quantity
 """
+from Functions import update_available_boxes
 
 class State:
     def __init__(self, residualSpaceList):
@@ -14,13 +15,17 @@ class State:
         self.planListBlock = []
         self.utilization = 0
         self.available = {}
+        self.space_volume = 0
+        self.block_volume = 0
 
     # TODO rest of the three methods maybe has the similar function?
     def add_block_planListBlock(self, block):
         self.planListBlock.append(block)
-
+        self.block_volume += block.get_volume()
+    
     def add_space_planListSpace(self, space):
         self.planListSpace.append(space)
+        self.space_volume += (space.get_size()[0] * space.get_size()[1] * space.get_size()[2])
 
     def get_residualSpaceList(self):
         return self.residualSpaceList
@@ -37,18 +42,20 @@ class State:
     def get_available_items(self):
         return self.available
 
+    def update_available_items(self, block):
+        self.available = update_available_boxes(self.available, block)
 
     def set_available_items(self, available):
         self.available  = available
-        return self.available
 
     def set_residualSpaceList(self, residualSpaceList):
         self.residualSpaceList = residualSpaceList
-        return self.residualSpaceList
 
-    def set_utilization(self,utilization):
-        self.utilization = utilization
-        return self.utilization
+    def update_utilization(self):
+        if self.space_volume == 0:
+            self.utilization = 0
+        else:
+            self.utilization = self.block_volume/self.space_volume
 
     def __repr__(self):
         return "{res: %s\n, occu:%s\n, fill: %s\n, ut: %s\n, avail:%s\n" \
