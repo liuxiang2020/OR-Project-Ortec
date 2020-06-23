b23ce884b4a61c95b8938427d544568a44bcfac8
Kevin Kruse <kevin.kruse@rwth-aachen.de>
2020-06-23 21:37:37 +0200

    #13 tried to combine residual spaces, doesn't work

--- find_solution.py
+++ find_solution.py
@@ -21,6 +21,58 @@ CONTAINER_SIZE: [L,W,H]
 available: dictionary, key:value  <-> id:quantity
 """
 
+def recombine_residual_spaces(space_list):
+    #Idea: If two spaces are nearby, but split up, a big space is available, but 
+    #not used by the algorithm
+
+    for space1 in space_list:
+        for space2 in space_list:
+            possible_space_size = [0,0,0]
+            possible_space_corner = [0,0,0]
+            #Collect equal corners
+            corner_eq = []
+            if (space1.corner[0] == space2.corner[0]):
+                corner_eq.append(True)
+            else:
+                corner_eq.append(False)
+                
+            if (space1.corner[1] == space2.corner[1]):
+                corner_eq.append(True)
+            else:
+                corner_eq.append(False)
+                
+            if (space1.corner[2] == space2.corner[2]):
+                corner_eq.append(True)
+            else:
+                corner_eq.append(False)
+            print(corner_eq)
+                
+                
+            # Length 3 shouldn't occur at all
+            if (corner_eq.count(True) == 1):
+                print("Starting recombination check")
+                for dim in range(3):
+                    if (corner_eq[dim] == False):
+                        if(space1.corner[dim] < space2.corner[dim]):
+                            if(space1.corner[dim]+space1.size[dim] > space2.corner[dim]):
+                                possible_space_size[dim] = (space1.corner[dim] + space1.size[dim]) - space2.corner[dim]
+                                possible_space_corner[dim] = (space1.corner[dim] + (space1.size[dim] - possible_space_size[dim]))
+                        else:
+                            if(space2.corner[dim]+space2.size[dim] > space1.corner[dim]):
+                                possible_space_size[dim] = (space2.corner[dim] + space2.size[dim]) - space1.corner[dim]
+                                possible_space_corner[dim] = (space2.corner[dim] + (space2.size[dim] - possible_space_size[dim]))
+                    if (corner_eq[dim] == True):
+                        possible_space_size[dim] = min(space1.size[dim], space2.size[dim])
+                        possible_space_corner[dim] = space1.corner[dim]
+                if ((possible_space_size[0] * possible_space_size[1] * possible_space_size[2]) > 0):
+                    # No space transfer on this experimental part
+                    print("OH DAMN BOY")
+                    exit()
+                    space_list.append(possible_space_corner, possible_space_size, 'z')
+                
+                    
+    
+
 def find_solution(itemKinds, container_size):
     # create available items
     available_items = {}
@@ -50,6 +102,8 @@ def find_solution(itemKinds, container_size):
             #packed_block = candidate_list[0]
             packState.add_block_to_space(packed_block, considered_space)
             space_list = create_residual_space(packed_block, space_list)
+            recombine_residual_spaces(space_list)
+            print(space_list)
         else:
             if len(space_list) <= 1:
                 break
