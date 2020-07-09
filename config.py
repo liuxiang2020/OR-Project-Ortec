9888d2decdcbaed315bcd3a06aca2711dd2b7aab
Kevin Kruse <64485282+kevin-kruse@users.noreply.github.com>
2020-07-09 19:02:02 +0200

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ MAX_GB = 4 # Used in create_blocks.py, sets the number of general block combinat
 K = 10 # at every stage the k best solutions are used for the next layers treesearch, number of (m,1) trees
 M_Zero = 4 # Size of the (m,1) trees, branching factor
 SCALE = 3 #...
-STAGE_L = 5 #...
-MAX_SIZE = 19 # Number of candidate blocks taken as start of the tree search
+STAGE_L = 4 #...
+MAX_SIZE = 18 # Number of candidate blocks taken as start of the tree search
 Parallel = False
 
 #Should be removed from config file
